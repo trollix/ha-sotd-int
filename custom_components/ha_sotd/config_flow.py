9d1db186ae2829b88626d2934738fa695631ee21
Trollix <trollix@gmail.com>
2025-04-06 23:00:00 +0200

    v0.3.0

--- custom_components/ha_sotd/config_flow.py
+++ custom_components/ha_sotd/config_flow.py
@@ -11,24 +11,23 @@ LANGUAGES = {
     "es": "Español"
 }
 
+CONFIG_SCHEMA = vol.Schema({
+    vol.Required(CONF_NAME, default=DEFAULT_NAME): str,
+    vol.Required("language", default=DEFAULT_LANG): vol.In(LANGUAGES),
+})
+
 class SotdConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
         if user_input is not None:
-            return self.async_create_entry(
-                title=user_input[CONF_NAME],
-                data=user_input,
-                options={
-                    "language": user_input.get("language", DEFAULT_LANG),
-                    "dummy_option": "show_config"
-                }
-            )
+            return self.async_create_entry(title=user_input[CONF_NAME], data=user_input)
+        return self.async_show_form(step_id="user", data_schema=CONFIG_SCHEMA)
 
-        return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema({
-                vol.Required(CONF_NAME, default=DEFAULT_NAME): str,
-                vol.Required("language", default=DEFAULT_LANG): vol.In(LANGUAGES)
-            })
-        )
+    async def async_step_options(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
+        return self.async_show_form(step_id="options", data_schema=CONFIG_SCHEMA)
+
+    def async_get_options_flow(self):
+        return self
